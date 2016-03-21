8643c86259c101a63970d36a6d2b4f7f1bf7c73f
Dairon Medina <dairon.medina@gmail.com>
2016-03-20 20:53:05 -0400

    Fixed setup.py for pypi compatibility

--- setup.py
+++ setup.py
@@ -1,3 +1,21 @@
+"""
+=======
+Guajiro
+=======
+
+.. image:: https://raw.githubusercontent.com/codeadict/guajiro/master/artwork/logo.png
+
+**Guajiro** is a word used to designate people from countryside in Cuba.
+They are usually very hardworking people with lot of culture and traditions.
+What you're more likely to see when you search "guajiro" are Cuban cowboys wearing
+hand made hat, riding horses and smoking tobaccos.
+
+The aim of this project is to make a insanely fast high-productivity web services
+framework built with python 3 honouring this hardworking people that every day work hard
+in the land, but this time in the land of coding.
+
+Guajiro is pretty easy, check the docs on `Github <https://github.com/codeadict/guajiro>`_
+"""
 from os.path import dirname, join
 from setuptools import setup, find_packages
 
@@ -8,9 +26,11 @@ setup(
         name='Guajiro',
         version=version,
         url='https://github.com/codeadict/guajiro',
-        description='Insanely fast high-productivity web services framework built with python 3 asyncio features.',
-        long_description=open('README.md').read(),
+        description='Insanely fast high-productivity web services framework built with'
+                    'Python3 asyncio features.',
+        long_description=__doc__,
         author='Dairon Medina',
+        author_email='dairon.medina@gmail.com',
         maintainer='Dairon Medina',
         maintainer_email='dairon.medina@gmail.com',
         license='BSD',
@@ -21,8 +41,8 @@ setup(
             'console_scripts': ['guajiro = guajiro.cmdline:execute']
         },
         classifiers=[
-            'Framework :: Guajiro',
-            'Environment :: Console',
+            'Development Status :: 4 - Beta',
+            'Environment :: Web Environment',
             'Intended Audience :: Developers',
             'License :: OSI Approved :: BSD License',
             'Operating System :: OS Independent',
